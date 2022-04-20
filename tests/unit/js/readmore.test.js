24aedc76c20caf9d34f484d2dd3d1bebd0e3d51d
Philippos Priovolos <prio_01@hotmail.com>
2022-04-20 20:47:50 +0300

    create new test

--- tests/unit/js/readmore.test.js
+++ tests/unit/js/readmore.test.js
@@ -1,5 +1,5 @@
 import { initClampers } from '../../../openlibrary/plugins/openlibrary/js/readmore';
-
+import $ from 'jquery'
 describe('initClampers', () => {
     test('clamp removed if not needed', () => {
         const clamper = document.createElement('div');
@@ -28,8 +28,42 @@ describe('initClampers', () => {
         expect(clamper.classList.contains('clamp')).toBe(true);
 
     });
+    const DUMMY_HTML=`<span class="clamp" data-before="▸ " style="display: unset;">
+    <h6>Subjects</h6>
+      <a>Ghosts</a>
+      <a>Monsters</a>
+      <a>Vampires</a>
+      <a>Witches</a>
+      <a>Challenges and Overcoming Obstacles</a>
+      <a>Magic and Supernatural</a>
+      <a>Cleverness</a>
+      <a>School Life</a>
+      <a>school stories</a>
+      <a>Wizards</a>
+      <a>>Magic</a>
+      <a>>MAGIA</a>
+      <a>>MAGOS</a>
+      <a>Juvenile fiction</a>
+      <a>Fiction</a>
+      <a>NOVELAS INGLESAS</a>
+      <a>Schools</a>
+      <a>orphans</a>
+      <a>fantasy fiction</a>
+      <a>England in fiction</a>
+      </span>`;
+    test('Clicking anchor tag does not expand',() =>{
+        const $clamper =$(DUMMY_HTML);
+        initClampers($clamper);
+        $clamper.find('a').first().on();
+        expect($clamper.css('display')).toBe('unset');
+    });
 
+    test('Clicking non-anchor tag does clamp',() => {
+        const $clamper =$(DUMMY_HTML);
+        initClampers($clamper);
+        $clamper.find('h6').first().on();
+        expect($clamper.css('display')).not.toBe('unset');
 
-
+    });
 
 });
